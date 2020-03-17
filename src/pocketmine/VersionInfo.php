51d0571873d8dcb8fc38ca8041ef69d494b5634d
Lawless1011 <54257675+Lawless1011@users.noreply.github.com>
2020-03-18 07:44:52 +0900

    ReirouEngine

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -32,7 +32,7 @@ if(defined('pocketmine\_VERSION_INFO_INCLUDED')){
 }
 const _VERSION_INFO_INCLUDED = true;
 
-const NAME = "PocketMine-MP";
-const BASE_VERSION = "3.11.7";
+const NAME = "ReirouEngine";
+const BASE_VERSION = "1.0.0";
 const IS_DEVELOPMENT_BUILD = true;
 const BUILD_NUMBER = 0;
